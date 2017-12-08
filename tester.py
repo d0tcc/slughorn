17cf5099412107a4d8731edb919797790515f47d
Julian Schrittwieser <schrittwieser@me.com>
2017-12-08 14:57:57 +0100

    adapted TwitterScraper to a weekly grouped scraping of tweets for performance reasons

--- tester.py
+++ tester.py
@@ -6,9 +6,10 @@ locale.setlocale(locale.LC_ALL, 'en_US')
 twitter_name = "realdonaldtrump"
 twitter_scraper = TwitterScraper(twitter_name)
 
-from_date = datetime(2017, 12, 6)
+from_date = datetime(2017, 11, 27)
 to_date = datetime(2017, 12, 7)
 
-twitter_scraper.scrape_all()
+# twitter_scraper.scrape_all()
+twitter_scraper.scrape_timeframe(from_date, to_date)
 print(twitter_scraper.tweets)
 print("Size:", len(twitter_scraper.tweets))
