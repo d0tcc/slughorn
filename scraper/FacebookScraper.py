b801b9b53bafb1f7f93d1ce94cc79a25bf892533
Julian Schrittwieser <schrittwieser@me.com>
2018-01-03 16:08:57 +0100

    working timeframe scraper (only 100 posts in this time frame)

--- scraper/FacebookScraper.py
+++ scraper/FacebookScraper.py
@@ -2,7 +2,11 @@ import facebook
 from datetime import datetime, timedelta
 import redis
 import logging
-from constants import facebook_access_token
+import requests
+from bs4 import BeautifulSoup
+import urllib3
+import re
+from scraper.constants import facebook_access_token
 logging.basicConfig(format='%(asctime)s [%(levelname)-5.5s]  %(message)s', level=logging.INFO)
 log = logging.getLogger(__name__)
 
@@ -27,27 +31,45 @@ class FacebookScraper:
 
     def scrape_all(self):
 
+        pass
+
+    def scrape_timeframe(self, from_date, to_date):
+
         graph = facebook.GraphAPI(
             access_token=facebook_access_token,
             version="2.5")
 
-        site = graph.get_objects(ids=[self.numeric_id], fields="feed")
-        posts_data = site[self.numeric_id]['posts']['data']
-        posts = [post['message'] for post in posts_data]
+        url = create_url(self.numeric_id, from_date, to_date)
+        site = graph.request(url)
+
+        posts_data = site['data']
+        posts = [post.get('message', post.get('story', '')) for post in posts_data]
         self.posts = posts
         return len(posts)
 
-    def scrape_timeframe(self, from_date, to_date):
-        return 0
-
     def find_numeric_id(self):
-        return 0
+
+        url = 'https://www.facebook.com/' + self.user_name
+        http_pool = urllib3.connection_from_url(url)
+        r = http_pool.urlopen('GET', url)
+        html = r.data.decode('utf-8')
+        try:
+            match = re.search(r"profile_id=(\d*)", html)
+            numeric_id = match.group(1)
+            return numeric_id
+        except (AttributeError, TypeError, KeyError, ValueError):
+            log.error("Numeric ID not found, returning 0")
+            return 0
+
 
 def format_date(date):
 
     return date.strftime('%Y-%m-%d')
 
 
-def create_url(user_name, from_date, to_date):
+def create_url(numeric_id, from_date, to_date):
 
-    return 0
+    from_string = format_date(from_date)
+    to_string = format_date(to_date)
+    url = "/{}/posts?limit=100&since={}&until={}".format(numeric_id, from_string, to_string)
+    return url
--- tester.py
+++ tester.py
@@ -1,4 +1,5 @@
 from scraper.TwitterScraper import TwitterScraper
+from scraper.FacebookScraper import FacebookScraper
 from datetime import datetime
 import locale
 import sys
@@ -8,13 +9,21 @@ if "linux" in sys.platform:
 else:
     locale.setlocale(locale.LC_ALL, 'en_US')
 
-twitter_name = "realdonaldtrump"
-twitter_scraper = TwitterScraper(twitter_name)
-
-from_date = datetime(2017, 11, 27)
+# twitter_name = "realdonaldtrump"
+# twitter_scraper = TwitterScraper(twitter_name)
+#
+from_date = datetime(2017, 9, 27)
 to_date = datetime(2017, 12, 7)
+#
+# # twitter_scraper.scrape_all()
+# twitter_scraper.scrape_timeframe(from_date, to_date)
+# print(twitter_scraper.tweets)
+# print("Size:", len(twitter_scraper.tweets))
 
-# twitter_scraper.scrape_all()
-twitter_scraper.scrape_timeframe(from_date, to_date)
-print(twitter_scraper.tweets)
-print("Size:", len(twitter_scraper.tweets))
+facebook_name = 'arminwolf.journalist'
+facebook_scraper = FacebookScraper(facebook_name, 1234, numeric_id=360686647276544)
+facebook_scraper.scrape_timeframe(from_date, to_date)
+print("Size:", len(facebook_scraper.posts))
+for post in facebook_scraper.posts:
+    print(post)
+    print("------------------")
